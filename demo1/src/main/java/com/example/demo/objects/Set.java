6163229191b83a0e46b788ad5f17fbdccf50ce5a
Marinel <102971709+Marinel-V@users.noreply.github.com>
2023-03-15 13:17:55 -0400

    added the protoype project to the main branch

---
+++ demo1/src/main/java/com/example/demo/objects/Set.java
@@ -0,0 +1,13 @@
+package com.example.demo.objects;
+
+public class Set {
+    private int number;
+    private int reps;
+    private String note;
+
+    public Set(int number){
+        this.number = number;
+        reps = 0;
+        note = "";
+    }
+}
